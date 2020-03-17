48483cd0cc47d5b91b3662905c72c077ea245747
Peter Glennon <pcrglennon@gmail.com>
2020-03-17 13:43:03 -0400

    Add tailwind, v1 of cross-text util

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [crossTextInput, setCrossTextInput] = useState('');
+  const [crossTextOutput, setCrossTextOutput] = useState('');
+
+  function handleCrossTextInputChange(e) {
+    const inputText = e.target.value;
+    setCrossTextInput(inputText);
+    setCrossTextOutput(crossText(inputText));
+  }
+
+  function crossText(inputText) {
+    const uppercasedCharacters = inputText.toUpperCase().split('');
+    let transformedText = '';
+
+    // horizontal axis
+    transformedText = uppercasedCharacters.join(' ');
+
+    // vertical axis
+    for (const char of uppercasedCharacters.slice(1)) {
+      transformedText += `\n${char}`;
+    }
+
+    return transformedText;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto">
+      <h1 className="mt-4">Text utils</h1>
+
+      <div className="border border-black mt-6 p-4">
+        <h3>Cross text</h3>
+
+        <div className="flex">
+          <div className="w-1/2">
+            <h5>Input:</h5>
+
+            <input
+              type="text"
+              value={crossTextInput}
+              onChange={e => handleCrossTextInputChange(e)}
+              className="border border-black p-2 w-1/2"
+            />
+          </div>
+
+          <div className="w-1/2">
+            <h5>Output:</h5>
+
+            <div className="bg-gray-300 p-2">
+              <p className="whitespace-pre-wrap">{crossTextOutput}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
